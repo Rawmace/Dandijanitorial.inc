7a255c6cb9ea4c7c01b8ffc09cd5c8f70344b5d8
Ramesh   Devkota <drhsemar@gmail.com>
2024-12-27 13:16:00 -0500

    server connection

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://dandijanitorial-inc.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
